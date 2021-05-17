1ebabfd52031d0551d5157bdfc93ba35743ecb16
EC2 Default User <ec2-user@ip-172-31-38-48.ap-northeast-2.compute.internal>
2021-05-17 04:45:30 +0000

    restart.sh

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def hello():
     image_value="https://post-phinf.pstatic.net/MjAxOTEyMzFfMTQz/MDAxNTc3NzU4NjE3ODE5.hyQQVHg1LCrJ1Y_J1NPfFIXFt8PXajxhLVxiV2ImdPcg.cHN_veuJwQcRzE8oHOfxjlz_AUXds5sRFrDRBtaEMQ0g.JPEG/%ED%8E%AD%EC%88%98%ED%95%98.jpg?type=w1200"
     return_value="<img src=\""+image_value+"\">"
     print(return_value)
-    return return_value 
+    return return_value
+@app.route('/downfile/woowahan')
+def down_woowahan():
+    file_woowahan='/home/ec2-user/uwsgi_server/download/woowahan.zip'
+    return file_woowahan
+ 
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
